9c96a7741211e11c9a5a09b04107b77371531c11
Felipe Nava <felipe_nava@hotmail.com>
2022-09-15 22:26:48 +0200

    Bugfix
    Prevent the snake from turning in on itself
    Prevent food from spawning on the snake

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public BoxCollider2D gridArea;
+    private Snake snake;
+
+    private void Awake()
+    {
+        snake = FindObjectOfType<Snake>();
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -11,12 +18,31 @@ public class Food : MonoBehaviour
 
     private void RandomizePosition()
     {
+        Vector3 foodPosition = SortPosition();
+
+        // While the food occupies the same sanek position, sort new food position.
+        while(snake.Occupies(foodPosition.x, foodPosition.y))
+        {
+            foodPosition = SortPosition();
+        }
+
+        // Assign the final position
+        this.transform.position = foodPosition;
+    }
+
+    private Vector3 SortPosition()
+    {
         Bounds bound = this.gridArea.bounds;
-        this.transform.position = new Vector3(
-             Mathf.Round(Random.Range(bound.min.x, bound.max.x)),
-             Mathf.Round(Random.Range(bound.min.y, bound.max.y)),
-             0.0f
-        ); 
+
+        // Pick a random position inside the bounds
+        float x = Random.Range(bound.min.x, bound.max.x);
+        float y = Random.Range(bound.min.y, bound.max.y);
+
+        // Round the values to ensure it aligns with the grid
+        x = Mathf.Round(x);
+        y = Mathf.Round(y);
+
+        return new Vector3(x, y, 0.0f);
     }
 
     void OnTriggerEnter2D(Collider2D other)
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    private Vector2 _direction = Vector2.right;
-    private List<Transform> _segments = new List<Transform>();
+    private Vector2 direction = Vector2.right;
+    private List<Transform> segments = new List<Transform>();
     public Transform segmentPrefab; 
     public int initialSize;
+    private Vector2 currentInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,50 +18,66 @@ public class Snake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        // It means that the snake is moving weather to the right or to the left side.
+        // So permit the snake only go up or down.
+        if (this.direction.x != 0.0f)
         {
-            _direction = Vector2.up;
-        } else if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                currentInput = Vector2.up;
+            } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                currentInput = Vector2.down;
+            }
+        // It means that the snake is moving weather to the right or to the left side.
+        // So permit the snake only go left or right.
+        } else if (this.direction.y != 0.0f)
         {
-            _direction = Vector2.down;
-        } else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
-        } else if(Input.GetKeyDown(KeyCode.A))
-        {
-            _direction = Vector2.left;
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                currentInput = Vector2.right;
+            } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                currentInput = Vector2.left;
+            }
         }
     }
 
     void FixedUpdate()
     {
-        // the rest of the body movement.
-        for(int i = _segments.Count - 1; i > 0; --i)
+        // Update the direction based on current input from the user.
+        if (currentInput != Vector2.zero)
         {
-            _segments[i].position = _segments[i - 1].position;
+            direction = currentInput;
+        }
+
+        // the rest of the body movement.
+        for(int i = segments.Count - 1; i > 0; --i)
+        {
+            segments[i].position = segments[i - 1].position;
         }
 
         // head movement.
         this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x) + _direction.x,
-            Mathf.Round(this.transform.position.y) + _direction.y,
+            Mathf.Round(this.transform.position.x) + direction.x,
+            Mathf.Round(this.transform.position.y) + direction.y,
             0.0f
         );
     }
 
     private void ResetState()
     {
-        for(int i = 1; i < _segments.Count; ++i)
+        for(int i = 1; i < segments.Count; ++i)
         {
-            Destroy(_segments[i].gameObject);
+            Destroy(segments[i].gameObject);
         }
 
-        _segments.Clear();
-        _segments.Add(this.transform);
+        segments.Clear();
+        segments.Add(this.transform);
 
         for(int i = 1; i < this.initialSize; ++i)
         {
-            _segments.Add(Instantiate(this.segmentPrefab));
+            segments.Add(Instantiate(this.segmentPrefab));
         }
 
         this.transform.position = Vector3.zero;
@@ -68,9 +86,9 @@ public class Snake : MonoBehaviour
     private void Grow()
     {
         Transform segment = Instantiate(this.segmentPrefab);
-        segment.position = _segments[_segments.Count - 1].position;
+        segment.position = segments[segments.Count - 1].position;
 
-        _segments.Add(segment);
+        segments.Add(segment);
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -82,5 +100,17 @@ public class Snake : MonoBehaviour
         {
             ResetState();
         }
+    }
+
+    public bool Occupies(float x, float y)
+    {
+        foreach(Transform segment in segments)
+        {
+            if (segment.position.x == x && segment.position.y == y)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
